4307c797808c3161b7682132e7ebbf6d5e2cc9ff
Carlos Ralf <carlosralf@gmail.com>
2025-03-18 16:10:51 -0300

    add

--- contabanco/src/ContaTerminal.java
+++ contabanco/src/ContaTerminal.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
+    /**
+     * @param args
+     * @throws Exception
+     */
+    @SuppressWarnings("resource")
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+        Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter username");
+    String nome = scanner.next();
+    System.out.println("Meu nome é " +nome);
+    
 }
