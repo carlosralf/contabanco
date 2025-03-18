8289e4b2614e828cebd38ec171d74da924ebbc9b
Carlos Ralf <carlosralf@gmail.com>
2025-03-18 20:17:05 -0300

    Adcionado a logica e impressão na tela

--- contabanco/src/ContaTerminal.java
+++ contabanco/src/ContaTerminal.java
@@ -1,18 +1,27 @@
 import java.util.Scanner;
 
 public class ContaTerminal {
+ 
     /**
      * @param args
-     * @throws Exception
      */
-    @SuppressWarnings("resource")
-    public static void main(String[] args) throws Exception {
+    @SuppressWarnings({ "resource" })
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 // Entrada com as dados da conta
-    System.out.println("Enter username");
+    System.out.println("Digite sua Agencia");
+    Integer Agencia = scanner.nextInt();
+
+    System.out.println("Digite sua conta");
+    String Conta = scanner.next();
+
+    System.out.println("Digite seu Nome");
     String nome = scanner.next();
 
-// Saida dos dados
-    System.out.println("Meu nome é " +nome);
-    
+    System.out.println("Digite seu Saldo");
+    final float Saldo = scanner.nextFloat();
+
+    // Saida dos dados
+        System.out.println("Olá ".concat(nome).concat(", obrigado por criar uma conta em nosso banco, sua agência é ")+Agencia +", conta ".concat(Conta).concat(" e seu saldo é: ")+Saldo +" já está disponível para saque");
+        }
 }
