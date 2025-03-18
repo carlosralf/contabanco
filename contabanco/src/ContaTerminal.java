32a3dcf0a78312015b3caf440fa018c560210e37
Carlos Ralf <carlosralf@gmail.com>
2025-03-18 16:13:42 -0300

    Comentários

--- contabanco/src/ContaTerminal.java
+++ contabanco/src/ContaTerminal.java
@@ -8,8 +8,11 @@ public class ContaTerminal {
     @SuppressWarnings("resource")
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
+// Entrada com as dados da conta
     System.out.println("Enter username");
     String nome = scanner.next();
+
+// Saida dos dados
     System.out.println("Meu nome é " +nome);
     
 }
